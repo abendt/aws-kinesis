ed1115ee8b52c20c2f122a22529264a0e5baeb7f
Alphonse Bendt <alphonse.bendt@gmail.com>
2023-12-24 18:40:31 +0100

    add credentials provider

--- spring-cloud-stream/src/test/kotlin/sample/KinesisConsumerSpec.kt
+++ spring-cloud-stream/src/test/kotlin/sample/KinesisConsumerSpec.kt
@@ -6,10 +6,18 @@ import io.kotest.extensions.spring.SpringExtension
 import io.mockk.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.cloud.stream.binder.test.InputDestination
 import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.messaging.support.GenericMessage
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
+import software.amazon.awssdk.services.kinesis.KinesisAsyncClient
+import software.amazon.awssdk.services.kinesis.KinesisClient
 
 @SpringBootTest
 @Import(TestChannelBinderConfiguration::class)
@@ -21,6 +29,16 @@ class KinesisConsumerSpec : StringSpec() {
 
     @MockkBean
     lateinit var myService: MyService
+
+    @TestConfiguration
+    class MyConfiguration {
+
+        /**
+         * We need to provide some dummy credentials as auto config will otherwise fail on GitHub
+         */
+        @Bean
+        fun awsCredentialsProvider(): AwsCredentialsProvider = StaticCredentialsProvider.create(AwsBasicCredentials.create("dummy", "dummy"))
+    }
 
     init {
         "can send event" {
